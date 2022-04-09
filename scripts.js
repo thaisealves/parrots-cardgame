fff596a1ae4edb52bc6f68e28a337f10e66bcdef
thaisealves <alves.thaisesilva@gmail.com>
2022-04-09 20:23:31 -0300

    Cartas virando com hover

--- scripts.js
+++ scripts.js
@@ -1,21 +1,31 @@
-function numCard(){
+function numCard() {
     let quantity = prompt("Com quantas cartas você quer jogar? (4 a 14)");
-    while(Number(quantity)<4 || Number(quantity)>14 || Number(quantity)%2 !== 0){
+    while (Number(quantity) < 4 || Number(quantity) > 14 || Number(quantity) % 2 !== 0) {
         quantity = prompt("Seu número deve ser par e entre 4 e 14");
     }
-  
+
     let list = document.querySelector(".cards");
     console.log(list)
-    for (let i = 0; i < Number(quantity); i++){
-        list.innerHTML += 
-        `<button onclick="hideFront(this)" >
-            <img src="/images/front.png" class="card card-front">
+    for (let i = 0; i < Number(quantity); i++) {
+        list.innerHTML +=
+        `<button class="card" onclick="hideFront(this)">
+        <div >
+            <div  class="card-front">
+            <img src="/images/front.png">
+            </div>
             
-        </button>`
+            <div class="card-back">
+            <img src="/images/metalparrot.gif" >
+            </div>
+        </div>
+        </button>           
+        `
     }
 }
 numCard()
 
-function hideFront(element){
-    element.classList.toggle("hide-front")
+function hideFront(element) {
+    element.classList.add("hide-front")
+
+
 }
