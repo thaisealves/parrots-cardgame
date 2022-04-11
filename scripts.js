e1c0a405a4ad37ce20cbe0edb510501d2429ddf9
thaisealves <alves.thaisesilva@gmail.com>
2022-04-11 02:50:04 -0300

    Comparando as cartas e desvirando

--- scripts.js
+++ scripts.js
@@ -4,26 +4,34 @@ while (Number(quantity) < 4 || Number(quantity) > 14 || Number(quantity) % 2 !==
 }
 
 let turned = [];
+let turnedImg;
 
-// putting the names twice so we can have te PAIR of cards we want, it has to always be in pairs so the memory game makes sense... 
-let backs = ["images/parrot1","images/parrot1", "images/parrot2", "images/parrot2", "images/parrot3","images/parrot3", "images/parrot4","images/parrot4","images/parrot5","images/parrot5",
-"images/parrot6","images/parrot6","images/parrot7","images/parrot7"]
+// putting the names twice so we can have te PAIR of cards we want, it has to always be in pairs so the memory game makes sense... source from the back of the cards
+let backs = ["images/parrot1", "images/parrot1", "images/parrot2", "images/parrot2", "images/parrot3", "images/parrot3", "images/parrot4", "images/parrot4", "images/parrot5", "images/parrot5",
+    "images/parrot6", "images/parrot6", "images/parrot7", "images/parrot7"]
+
+// for (let j = 1; j < 8; j++) {
+//     let imgs = [{
+//         name: `parrot${j}`,
+//         source: `images/parrot${j}.gif`
+//     }]
+//     console.log(imgs)
+// }
 
 //"cutting" from the first indice till it gets the number we passed on the quantity
 let cardsShuffle = backs.slice(0, quantity);
 cardsShuffle.sort(comparing)
 
-function comparing() { 
-	return Math.random() - 0.5; 
+function comparing() {
+    return Math.random() - 0.5;
 }
 
 function numCard() {
-
-
     let list = document.querySelector(".cards");
     console.log(list)
+
     for (let i = 0; i < cardsShuffle.length; i++) {
-       //here we'll have the number of cards we asked, because it's the length from the part that we sliced from the array 
+        //here we'll have the number of cards we asked, because it's the length from the part that we sliced from the array 
         list.innerHTML +=
             `<button class="card">
         <div class="into-card" onclick="hideFront(this)" >
@@ -35,41 +43,39 @@ function numCard() {
             <img src="${cardsShuffle[i]}.gif" >
             </div>
         </div>
-           </button>   
-
-        
+           </button> 
         `
     }
 }
 numCard()
 
+let clicked = [];
+
 function hideFront(element) {
     element.classList.add("flipped")
-    
-    let turnedImg = document.querySelector(".flipped").querySelectorAll("img")
 
-    
-    turned.push(turnedImg)
-    console.log(turned.length)
-    
+    turned.push(element.parentNode.innerHTML)
+    console.log(turned)
+    clicked.push(element)
+    console.log(clicked)
 
-    let imgTurned = (turned[turned.length - 1])
+
     let imgTurned2 = (turned[turned.length - 2])
+    let imgTurned = (turned[turned.length - 1])
 
-console.log(turned.length)
-    console.log(imgTurned[1].src)
-    console.log(imgTurned2[1].src)
+    console.log(imgTurned)
+    console.log(imgTurned2)
 
-    if (turned.length - 2 >= 0 && imgTurned[1].src !== imgTurned2[1].src) {
-        setTimeout(unflipped, 1000)
+
+    if (turned.length - 2 >= 0 && imgTurned !== imgTurned2 && clicked.length%2 === 0) {
+
+        setTimeout(function () { 
+        clicked[clicked.length-1].classList.remove("flipped")
+        clicked[clicked.length-2].classList.remove("flipped")
+
+    }, 1000)
     }
 
     // se as cartas não foram iguais, remove("flipped"), tirar o toggle e deixar só o add 
-
-}
-
-function unflipped() {
-   let unturning = document.querySelectorAll(".flipped")
-   console.log(unturning.length - 1)
 
 }
